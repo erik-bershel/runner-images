d009b042df5871324236c5e38e496114a4cc1a8a
Erik Bershel <erik-bershel@github.com>
2025-02-14 15:37:06 +0100

    tweak configure system

--- images/ubuntu/scripts/build/configure-system.sh
+++ images/ubuntu/scripts/build/configure-system.sh
@@ -7,6 +7,7 @@
 source $HELPER_SCRIPT_FOLDER/etc-environment.sh
 source $HELPER_SCRIPT_FOLDER/os.sh
 
+rm -rf /opt/post-generation
 mv -f /imagegeneration/post-generation /opt
 
 echo "chmod -R 777 /opt"
@@ -22,15 +23,6 @@ ENVPATH=${ENVPATH#"\""}
 ENVPATH=${ENVPATH%"\""}
 replace_etc_environment_variable "PATH" "${ENVPATH}"
 echo "Updated /etc/environment: $(cat /etc/environment)"
-
-# Clean yarn and npm cache
-if yarn --version > /dev/null; then
-    yarn cache clean
-fi
-
-if npm --version; then
-    npm cache clean --force
-fi
 
 if is_ubuntu24; then
 # Prevent needrestart from restarting the provisioner service.
